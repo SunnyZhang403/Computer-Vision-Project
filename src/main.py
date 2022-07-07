b703005eaedb906ad99ad35b532cf43d0a254169
SunnyZhang403 <54222067+SunnyZhang403@users.noreply.github.com>
2022-07-07 16:42:30 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,10 +5,11 @@ import argparse
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  subparsers = pareser.add_subparsers(dest='command', required = True)
+  subparsers = parser.add_subparsers(dest='command', required = True)
   
   parser_base = subparsers.add_parser('base')
-  parser_base.add_argument('--debug')
+  parser_base.set_defaults(func=hello_world)
+
   
   args = parser.parse_args()
   
