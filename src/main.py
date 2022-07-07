26adb673031475ce830e81cb6762e86c4de2fdfa
SunnyZhang403 <54222067+SunnyZhang403@users.noreply.github.com>
2022-07-07 16:32:32 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
+import sys
+from pathlib import path
+import argparse
 
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  subparsers = pareser.add_subparsers(dest='command', required = True)
+  
+  parser_base = subparsers.add_parser('base')
+  parser_base.add_argument('--debug')
+  
+  args = parser.parse_args()
+  
+  args.func(args)
+  
